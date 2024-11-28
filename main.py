17fe175369e2d05ee01a1b45625ab60f67f1496d
Ahmed  Ghallab <154532516+ahmedghallab22@users.noreply.github.com>
2024-11-28 10:26:41 +0200

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ serial = c.Win32_BaseBoard()[0].SerialNumber.strip()
 if (serial=="Default string"):
     print("my_code")
 else:
-    print("You are not authorized to use the program. Contact the owner of the program on WhatsApp: 01207059498")
+    print("You are not authorized to use the program. Contact the owner of the program on WhatsApp: 0123456789")
 
 
 
